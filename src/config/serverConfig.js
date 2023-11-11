9ac080af0e5cacdac86a5e31d03e24634f3dd7b2
Thilak <thilaksaithipparapu6300@gmail.com>
2023-11-11 20:56:39 +0530

    Signup logic with password encrpytion done

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,7 +1,9 @@
 const dotenv = require('dotenv');
+const bcrpyt = require('bcrypt');
 
 dotenv.config();
 
-module.exports={
-    PORT: process.env.PORT
+module.exports= {
+    PORT: process.env.PORT,
+    SALT: bcrpyt.genSaltSync(10)
 }
--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,10 @@
 const {
   Model
 } = require('sequelize');
+const bcrpyt = require('bcrypt');
+
+const { SALT } = require('../config/serverConfig');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -33,5 +37,10 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'User',
   });
+
+  User.beforeCreate((user) => {
+     const encryptedPassword = bcrpyt.hashSync(user.password, SALT);
+     user.password = encryptedPassword;
+  });
   return User;
-};
+};  
